da4ba6b82b46a7d1f0d0e190ffe56308edbfcd49
Felipe Arruda Pontes <felipe.arrudapontes@maastrichtuniversity.nl>
2026-05-11 11:35:15 +0200

    Changing from risk to something else. maymbe change the url as well?

--- src/components/NavClient.tsx
+++ src/components/NavClient.tsx
@@ -9,7 +9,7 @@ export default function NavClient() {
   return (
     <nav className="nav">
       <Link href="/" className="navLink">Home</Link>
-      <Link href="/risk-model" className="navLink">Risk Model</Link>
+      <Link href="/risk-model" className="navLink">Investment Priorities</Link>
       <Link href="/about" className="navLink">About</Link>
       <Link href="/support" className="navLink">Support</Link>
 
