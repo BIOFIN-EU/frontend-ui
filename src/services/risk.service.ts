28a37c54e0988058e556f8c5c9d7eb27bd277db0
Felipe Arruda Pontes <felipe.arrudapontes@maastrichtuniversity.nl>
2026-09-11 16:34:19 +0200

    for now using different host to fetch risk data. Need to see how this looks like using api gateway

---
+++ src/services/risk.service.ts
@@ -0,0 +1,63 @@
+// src/services/risk.service.ts
+import {
+  apiFetch,
+} from "@/lib/api";
+
+// const BASE = "/api/risk-framework";
+const BASE = "http://localhost:8009/api/v1";
+
+export type PriorityActionsCaseResponse = {
+  id: string;
+  riskData: any;
+  [key: string]: any; // anything for now
+};
+export type RiskScoreCaseResponse = {
+  id: string;
+  [key: string]: any; // anything for now
+};
+
+export async function getRiskScoreByID(riskCaseURI: string) {
+  // const data = await apiFetch<RiskScoreCaseResponse>(`${BASE}${riskCaseURI}`, {method: "GET"});
+
+  // 2. Fetch the risk data using the URL from the response
+  const riskRes = await fetch(`${BASE}${riskCaseURI.replace("/api/v1", "")}`);
+  if (!riskRes.ok) throw new Error(`Risk fetch error: ${riskRes.status}`);
+  const riskJson = await riskRes.json();
+  return riskJson;
+}
+
+export async function getPriorityManagementActions(country_code: string, polygon: string) {
+  // const data = await apiFetch<PriorityActionsCaseResponse>(`${BASE}/management-actions/priority/`, {
+  //   method: "POST",
+  //   body: JSON.stringify({
+  //     country_code: country_code,
+  //     // wkt_polygon: polygon,
+  //     risk_model: "EddamiriEtAl2026",
+  //     risk_type: "Full",
+  //     sri_logic_type: "fuzzy",
+  //     sri_correction_method: "HFI",
+  //   }),
+  // });
+
+  // 1. Fetch priority data
+  const priorityRes = await fetch(`${BASE}/management-actions/priority/`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      country_code: country_code,
+      // wkt_polygon: polygon,
+      risk_model: "EddamiriEtAl2026",
+      risk_type: "Full",
+      sri_logic_type: "fuzzy",
+      sri_correction_method: "HFI",
+    }),
+  });
+  if (!priorityRes.ok) throw new Error(`Priority fetch error: ${priorityRes.status}`);
+  const data: PriorityActionsCaseResponse = await priorityRes.json();
+
+  const riskRes = await getRiskScoreByID(data.risk);
+  data.riskData = riskRes
+  console.log(data);
+  return data;
+}
+
