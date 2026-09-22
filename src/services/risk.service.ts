aab1b968eaa7127ff6669e3ee668d562ad0b057e
Felipe Arruda Pontes <felipe.arrudapontes@maastrichtuniversity.nl>
2026-09-22 17:11:02 +0200

    removeing localhost

--- src/services/risk.service.ts
+++ src/services/risk.service.ts
@@ -3,9 +3,9 @@ import {
   apiFetch,
 } from "@/lib/api";
 
-// const BASE = "/api/risk-framework";
+const BASE = "/api/risk-framework";
 // const BASE = "http://localhost:8009/api/v1";
-const BASE = "http://localhost:8000/api/vulnerability";
+// const BASE = "http://localhost:8000/api/vulnerability";
 
 export type PriorityActionsCaseResponse = {
   id: string;
